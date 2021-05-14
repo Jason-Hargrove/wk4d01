1d929dd6c16339dcc979f86725cc6008c79c0109
Jason Hargrove <rijaha@gmail.com>
2021-05-14 12:06:06 -0500

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,6 +1,7 @@
+// our hero
 class Hero {
   constructor(name) {
-    this.health = 0;
+    this.health = 100;
     this.weapons = {
                       sprinkleySpray: 5,
                       shugarShock: 10
@@ -9,16 +10,47 @@ class Hero {
                          'you can\'t count my calories'];
   }
   talkSass() {
+    let arr = this.catchPhrases;
+    let randomSass = arr.[Math.floor(Math.random() * arr.length)];
+    return randomSass;
 
   }
   announceHealth() {
-
+    console.log(this.health);
   }
   fight() {
     console.log('i\'m ready to rumble');
   }
 }
 
+// instance of dougie the donut
 const dougie = new Hero('Dougie the Donut');
 
-console.log(dougie);
+
+// our enemy
+class Enemy{
+  constructor(name) {
+    this.health = 100;
+    this.weapons = {
+                      pepperoniStars: 5,
+                      cheeseGrease: 10
+                    };
+    this.catchPhrases = ['i\'m youtube famous',
+                         'i\'m more dangerous than an uncovered sewer'];
+  }
+  talkSmack() {
+    let arr = this.catchPhrases;
+    let randomSmack = arr.[Math.floor(Math.random() * arr.length)];
+    return randomSmack;
+  }
+  announceHealth() {
+    console.log(this.health);
+  }
+  fight() {
+    console.log('i\'m gonna flatten you like a slice of pepperoni!');
+  }
+}
+
+const pizzaRat = new Enemy('Pizza Rat');
+
+console.log(pizzaRat.talkSmack());
